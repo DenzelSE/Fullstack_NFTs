cd7a753156c2051448f80a6c143a8ad6d5d1ed94
DenzelSE <t.d.selokela@gmail.com>
2024-11-16 00:08:48 +0200

    contract change and deploy script

---
+++ backend/contracts/AvengersNFT.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract AvengersNFT is ERC721Enumerable{
+    uint256 public constant MAX_SUPPLY = 10;
+    uint256 public constant PRICE = 0.0004 ether;
+    uint256 public constant MAX_PER_WALLET = 5;
+
+    string private _baseTokenURI;
+
+    mapping(address => uint256) public walletMints;
+
+    constructor(string memory baseURI) ERC721("AvengersNFT", "AVN") {
+        _baseTokenURI = baseURI;
+    }
+
+    function mint(uint256 _amount) external payable {
+        require(totalSupply() + _amount <= MAX_SUPPLY, "Exceeds max supply");
+        require(walletMints[msg.sender] + _amount <= MAX_PER_WALLET, "Exceeds wallet limit");
+        require(msg.value >= PRICE * _amount, "Ether sent is not correct");
+
+        walletMints[msg.sender] += _amount;
+
+        for (uint256 i = 0; i < _amount; i++) {
+            uint256 tokenId = totalSupply() + 1;
+            _safeMint(msg.sender, tokenId);
+        }
+    }
+
+    function setBaseURI(string memory baseURI) public {
+        _baseTokenURI = baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
+}
---
+++ backend/scripts/deploy.js
@@ -0,0 +1,18 @@
+const {ethers} = require("hardhat");
+
+async function main() {
+  const baseURI = "ipfs://QmTYWKogeU2MALmVaAUFFM8LjNdJs8PULQpiRsSyoCkag9/";
+
+  const AvengersNFT = await ethers.getContractFactory("AvengersNFT");
+
+  const avengersNFTDeployed = await AvengersNFT.deploy(baseURI);
+
+  await avengersNFTDeployed.waitForDeployment();
+
+  console.log("AvengersNFT deployed to:", avengersNFTDeployed.target);
+}
+
+main()
+    .catch((error) => {console.error(error);
+        process.exit(1);
+});
