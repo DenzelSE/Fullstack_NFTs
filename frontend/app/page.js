edb80a08e2f991f97c40441ba94fc783a7a5c4a8
DenzelSE <t.d.selokela@gmail.com>
2024-11-18 14:28:35 +0200

    contract address

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { ethers } from "ethers";
 import AvengersNftABI from "../contract/AvengersNFT.json";
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = "0xCcC5386A74B867be5cFE073427272E1E24F98acB";
 
 export default function Home() {
   const [account, setAccount] = useState(null);
